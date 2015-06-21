20ae77d181117633376db3919d37a4d2928e41ec
Svemir Brkic <svemir@symplicity.com>
2015-06-21 10:08:47 -0400

    remove travis-specific stuff from karma for now

--- karma.conf.js
+++ karma.conf.js
@@ -25,19 +25,5 @@ module.exports = function(config) {
         }
     };
 
-    // This block is needed to execute Chrome on Travis
-    // If you ever plan to use Chrome and Travis, you can keep it
-    // If not, you can safely remove it
-    // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
-    if (configuration.browsers[0] === 'Chrome' && process.env.TRAVIS) {
-        configuration.customLaunchers = {
-            'chrome-travis-ci': {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
-        };
-        configuration.browsers = ['chrome-travis-ci'];
-    }
-
     config.set(configuration);
 };
