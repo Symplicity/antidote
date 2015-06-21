75d9f871a7e1544e6b8f9a4b6c20204a79aa749b
Svemir Brkic <svemir@symplicity.com>
2015-06-21 09:24:34 -0400

    fix intentation

--- karma.conf.js
+++ karma.conf.js
@@ -2,42 +2,42 @@
 
 module.exports = function(config) {
 
-  var configuration = {
-    autoWatch : false,
+    var configuration = {
+        autoWatch : false,
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
 
-    ngHtml2JsPreprocessor: {
-      stripPrefix: 'src/',
-      moduleName: 'gulpAngular'
-    },
+        ngHtml2JsPreprocessor: {
+            stripPrefix: 'src/',
+            moduleName: 'gulpAngular'
+        },
 
-    browsers : ['PhantomJS'],
+        browsers : ['PhantomJS'],
 
-    plugins : [
-      'karma-phantomjs-launcher',
-      'karma-jasmine',
-      'karma-ng-html2js-preprocessor'
-    ],
+        plugins : [
+          'karma-phantomjs-launcher',
+          'karma-jasmine',
+          'karma-ng-html2js-preprocessor'
+        ],
 
-    preprocessors: {
-      'src/**/*.html': ['ng-html2js']
-    }
-  };
-
-  // This block is needed to execute Chrome on Travis
-  // If you ever plan to use Chrome and Travis, you can keep it
-  // If not, you can safely remove it
-  // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
-  if(configuration.browsers[0] === 'Chrome' && process.env.TRAVIS) {
-    configuration.customLaunchers = {
-      'chrome-travis-ci': {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
+        preprocessors: {
+            'src/**/*.html': ['ng-html2js']
+        }
     };
-    configuration.browsers = ['chrome-travis-ci'];
-  }
 
-  config.set(configuration);
+    // This block is needed to execute Chrome on Travis
+    // If you ever plan to use Chrome and Travis, you can keep it
+    // If not, you can safely remove it
+    // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
+    if (configuration.browsers[0] === 'Chrome' && process.env.TRAVIS) {
+        configuration.customLaunchers = {
+            'chrome-travis-ci': {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        };
+        configuration.browsers = ['chrome-travis-ci'];
+    }
+
+    config.set(configuration);
 };
--- protractor.conf.js
+++ protractor.conf.js
@@ -4,22 +4,22 @@ var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
 
 // An example configuration file.
 exports.config = {
-  // The address of a running selenium server.
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
+    // The address of a running selenium server.
+    //seleniumAddress: 'http://localhost:4444/wd/hub',
+    //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome'
+    },
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: [paths.e2e + '/**/*.js'],
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: [paths.e2e + '/**/*.js'],
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    }
 };
