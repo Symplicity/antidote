2dadcb2f0bb6a0daff02b2329247e803de71be88
Bikramjit Singh <bsingh@symplicity.com>
2015-06-22 17:29:17 -0400

    changed name to clarify gulp task is api docs, and output to api-docs since there is more type of documentation in the repo now

--- gulp/api-docs.js
+++ gulp/api-docs.js
@@ -4,8 +4,8 @@ var gulp = require('gulp');
 var aglio = require('gulp-aglio');
 var path = require('path');
 
-gulp.task('docs', function() {
+gulp.task('api-docs', function() {
     gulp.src(path.join(__dirname, '/../docs/api/*.apib'))
-    .pipe(aglio({template: 'default'}))
-    .pipe(gulp.dest(path.join(__dirname, '/../public/docs')));
+        .pipe(aglio({template: 'default'}))
+        .pipe(gulp.dest(path.join(__dirname, '/../public/api-docs')));
 });
