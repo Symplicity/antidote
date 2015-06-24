c7e33b9b41f099f0b81cab03ec0605dfb7d3d193
Bikramjit Singh <bsingh@symplicity.com>
2015-06-24 19:32:40 -0400

    remove need for temporary concepts table during migration - will use if there else fake the data using factory

--- database/seeds/DrugSeeder.php
+++ database/seeds/DrugSeeder.php
@@ -10,40 +10,50 @@ class DrugSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        /* TODO: fetch concepts data from API in this seeder - currently using db table until that is ready */
-        $concepts = DB::select('select * from concepts');
+    public function run() {
+        if (Schema::hasTable('concepts')) {
+            /* TODO: fetch concepts data from API in this seeder - currently using db table until that is ready */
+            $concepts = DB::select('select * from concepts');
 
-        foreach ($concepts as $concept) {
-            $data = json_decode($concept['data'], true);
+            foreach ($concepts as $concept) {
+                $data = json_decode($concept['data'], true);
 
-            Drug::create([
-                'label' => ucfirst($data['name']),
-                'rxcui' => $data['rxcui'],
-                'generic' => ucfirst($data['generic']),
-                'drug_forms' => $data['drug_forms']
-            ]);
-        }
-
-        //add many-to-many relations
-        $drugs = DB::select('select rxcui, id from drugs');
-        $drug_map = array_column($drugs, 'id', 'rxcui');
-
-        foreach ($concepts as $concept) {
-            $data = json_decode($concept['data'], true);
-            $drug = Drug::where('rxcui', '=', $concept['rxcui'])->first();
-
-            if ($data['related'] && is_array($data['related'])) {
-                $related = $data['related'];
-                $rels = [];
-                foreach ($related as $related_drug_rxcui) {
-                    if (isset($drug_map[$related_drug_rxcui])) {
-                        $rels[] = $drug_map[$related_drug_rxcui];
-                    }
-                }
-                $drug->related()->sync($rels);
+                Drug::create([
+                    'label' => ucfirst($data['name']),
+                    'rxcui' => $data['rxcui'],
+                    'generic' => ucfirst($data['generic']),
+                    'drug_forms' => $data['drug_forms']
+                ]);
             }
+
+            //add many-to-many relations
+            $drugs = DB::select('select rxcui, id from drugs');
+            $drug_map = array_column($drugs, 'id', 'rxcui');
+
+            foreach ($concepts as $concept) {
+                $data = json_decode($concept['data'], true);
+                $drug = Drug::where('rxcui', '=', $concept['rxcui'])->first();
+
+                if ($data['related'] && is_array($data['related'])) {
+                    $related = $data['related'];
+                    $rels = [];
+                    foreach ($related as $related_drug_rxcui) {
+                        if (isset($drug_map[$related_drug_rxcui])) {
+                            $rels[] = $drug_map[$related_drug_rxcui];
+                        }
+                    }
+                    $drug->related()->sync($rels);
+                }
+            }
+        } else {
+            //fake data
+            factory('App\Drug', 50)->create()->each(function ($drug) {
+                $drug->sideEffects()->sync([1, 2, 3]);
+
+                $drug->alternatives()->sync([1, 2, 3]);
+
+                $drug->related()->sync([1, 2, 3]);
+            });
         }
     }
 }
