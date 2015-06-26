6495963c5720bb36224bfed9b6dc3e20ec5600cd
Svemir Brkic <svemir@symplicity.com>
2015-06-25 23:47:48 -0400

    verify email set/get on model

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,14 +2,24 @@
 
 class UserTest extends TestCase
 {
-    /**
-     * Checks whether user age attribute is correctly set
-     *
-     */
     public function testSetAgeAttribute()
     {
         $user = factory('App\User')->create(['age' => 34]);
 
         $this->assertSame(34, $user->age);
+    }
+
+    public function testSetEmailAttribute()
+    {
+        $user = factory('App\User')->create(['email' => 'foo@bar.com']);
+
+        $this->assertSame('foo@bar.com', $user->email);
+    }
+
+    public function testReviews()
+    {
+        $user = factory('App\User')->create(['email' => 'foo@bar.com']);
+        $reviews = $user->reviews();
+        $this->assertInstanceOf('\Illuminate\Database\Eloquent\Relations\HasMany', $reviews);
     }
 }
