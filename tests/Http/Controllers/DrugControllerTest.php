8cc67a88f78b3350be5f1d05b9d4c52f807562ae
Svemir Brkic <svemir@symplicity.com>
2015-06-25 23:35:45 -0400

    more drug controller tests

--- tests/Http/Controllers/DrugControllerTest.php
+++ tests/Http/Controllers/DrugControllerTest.php
@@ -32,12 +32,30 @@ class DrugControllerTest extends TestCase
     {
         $this->stubQuery->shouldReceive('paginate')->once()->with(15);
         $this->stubQuery->shouldReceive('orderBy')->once()->with('label', 'ASC')->andReturn($this->stubQuery);
-        $this->mockModel->shouldReceive('with')
-            ->once()
-            ->with('sideEffects')
-            ->andReturn($this->stubQuery);
+        $this->mockModel->shouldReceive('with')->once()->with('sideEffects')->andReturn($this->stubQuery);
 
         $request = new Illuminate\Http\Request;
+
+        $this->ctrl->index($request);
+    }
+
+    public function testIndexFull()
+    {
+        $params = [
+            'limit' => 20,
+            'keywords' => 'foo',
+            'alpha' => 'c'
+        ];
+        $this->stubQuery->shouldReceive('paginate')->once()->with(20);
+        $this->stubQuery->shouldReceive('where')->once()->with('label', 'LIKE', '%foo%')->andReturn($this->stubQuery);
+        $this->stubQuery->shouldReceive('orWhere')->once()->with('description', 'LIKE', '%foo%')->andReturn($this->stubQuery);
+        $this->stubQuery->shouldReceive('where')->once()->with('label', 'LIKE', 'c%')->andReturn($this->stubQuery);
+        $this->stubQuery->shouldReceive('orderBy')->once()->with('label', 'ASC')->andReturn($this->stubQuery);
+
+        $this->mockModel->shouldReceive('with')->once()->with('sideEffects')
+            ->andReturn($this->stubQuery);
+
+        $request = new Illuminate\Http\Request($params);
 
         $this->ctrl->index($request);
     }
