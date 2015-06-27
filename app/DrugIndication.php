15efea8872721b03c1e4ae4a5e6bed5f60d5d1e7
Bikramjit Singh <bsingh@symplicity.com>
2015-06-27 16:43:20 -0400

    remove 'pivot' attribute from JSON output as it makes the API fatter than needed

--- app/DrugIndication.php
+++ app/DrugIndication.php
@@ -8,6 +8,8 @@ class DrugIndication extends Model
 {
     protected $table = 'drug_indications';
 
+    protected $hidden = ['pivot'];
+
     public $timestamps = false;
 
     protected $fillable = [
