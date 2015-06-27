aed938f95a53f92b5c470d8710a6c26cf53e52c9
Svemir Brkic <svemir@symplicity.com>
2015-06-27 15:35:24 -0400

    add test for user update

--- tests/Http/Controllers/UserControllerTest.php
+++ tests/Http/Controllers/UserControllerTest.php
@@ -1,16 +1,20 @@
 <?php
 
+use App\Http\Controllers\UserController;
+
 class UserControllerTest extends TestCase
 {
+    /**
+     * @var UserController
+     */
     private $ctrl;
-    private $stubQuery;
     private $mockModel;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->ctrl = new \App\Http\Controllers\UserController;
+        $this->ctrl = new UserController;
 
         $this->mockModel = \Mockery::mock('\App\User');
         $this->app->instance('User', $this->mockModel);
@@ -22,6 +26,37 @@ class UserControllerTest extends TestCase
 
         $request = new Illuminate\Http\Request(['user' => ['sub' => 'foo']]);
 
-        $user = $this->ctrl->getUser($request);
+        $this->ctrl->getUser($request);
+    }
+
+    public function testUpdateUserSansPassword()
+    {
+        $this->mockModel->shouldReceive('find')->once()->with('foo')
+            ->andReturn((object) ['password' => 'foobar']);
+
+        $request = new Illuminate\Http\Request(['user' => ['sub' => 'foo']]);
+
+        Hash::shouldReceive('check')->once()->andReturn(false);
+
+        $response = $this->ctrl->updateUser($request);
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertContains('enter correct current password', $response->getContent());
+    }
+
+    public function testUpdateUser()
+    {
+        $this->mockModel->shouldReceive('getAttribute')->with('password');
+        $this->mockModel->shouldReceive('setAttribute');
+        $this->mockModel->shouldReceive('save')->once();
+        $this->mockModel->shouldReceive('find')->once()->with('foo')->andReturn($this->mockModel);
+
+        $request = new Illuminate\Http\Request(['user' => ['sub' => 'foo']]);
+
+        Hash::shouldReceive('check')->once()->andReturn(true);
+
+        $user = $this->ctrl->updateUser($request);
+
+        $this->assertInstanceOf('\App\User', $user);
     }
 }
