1858067090aedd54fcd2c8d8d4adf040f93a60df
Richard <richard@guthnur.net>
2015-06-28 12:20:07 -0400

    latest fixes to the images

--- deployment/worker/update.sh
+++ deployment/worker/update.sh
@@ -2,25 +2,13 @@
 
 echo "Running Update Now\n"
 
-MIGRATION=$1
-
-if [ "$ANTIDOTE_ROLE" = "web" ]; then
-  cd /var/www
-  echo "Pulling Latest code from $GITREPO_BRANCH"
-  git pull origin $GITREPO_BRANCH
-  unzip dist.zip /var/www/
-fi
+cd /var/www
+echo "Pulling Latest code from $GITREPO_BRANCH"
+git pull origin $GITREPO_BRANCH
+unzip -q dist.zip -d /var/www/
 
 if [ "$ANTIDOTE_ROLE" = "worker" ]; then
-  cd /var/www
-  echo "Pulling Latest code from $GITREPO_BRANCH"
-  git pull origin $GITREPO_BRANCH
-  unzip dist.zip /var/www/
   echo "Running Migrations"
-  if [ -z "$MIGRATION" ];then
-    php artisan $MIGRATION
-  else
-    php artisan migrate
-  fi
+  php artisan migrate --force
 fi
 /var/www/scripts/notify.sh SUCCESS "Deploy happened."
