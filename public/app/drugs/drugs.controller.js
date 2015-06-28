89020ee25623ca26ec90a4436546108daaa8e92c
Bikramjit Singh <bsingh@symplicity.com>
2015-06-28 12:58:11 -0400

    added chart example for front end to follow

--- public/app/drugs/drugs.controller.js
+++ public/app/drugs/drugs.controller.js
@@ -79,6 +79,13 @@
     function DrugsOverviewCtrl() {
         activate();
 
+        this.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+        this.series = ['Series A', 'Series B'];
+        this.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+        ];
+
         function activate() {
             //TODO: add API service call here
         }
