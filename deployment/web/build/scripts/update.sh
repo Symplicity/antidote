f783c32364c6dc2766285e6c8a84a05c6078d54c
Richard <richard@guthnur.net>
2015-06-29 18:52:00 -0400

    added nginx logging files for elasticsearch

--- deployment/web/build/scripts/update.sh
+++ deployment/web/build/scripts/update.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
+cd /var/www
 
 echo "Running Update Now\n"
+OLD_GITSHORT=`git rev-parse --short HEAD`
 
-cd /var/www
 echo "Pulling Latest code from $GITREPO_BRANCH"
 git pull origin $GITREPO_BRANCH
 rm -rf /var/www/{dist,vendor}
 unzip -q dist.zip -d /var/www/
-ln -s /worker/ /var/www/webhook/
+
+ln -s /worker/ /var/www/dist/webhook
+GITSHORT=`git rev-parse --short HEAD`
 
 if [ "$ANTIDOTE_ROLE" = "worker" ]; then
   echo "Running Migrations"
   php artisan migrate --force
 fi
-/var/www/scripts/notify.sh SUCCESS "Deploy happened ${HOSTNAME}"
+
+/var/www/scripts/notify.sh SUCCESS "Deployed! Was Running ${OLD_GITSHORT} now running ${GITSHORT}, happened on ${HOSTNAME}"
--- deployment/worker/build/scripts/update.sh
+++ deployment/worker/build/scripts/update.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-
-echo "Running Update Now\n"
-
 cd /var/www
+echo "Running Update Now\n"
+OLD_GITSHORT=`git rev-parse --short HEAD`
+
 echo "Pulling Latest code from $GITREPO_BRANCH"
 git pull origin $GITREPO_BRANCH
 rm -rf /var/www/{dist,vendor}
 unzip -q dist.zip -d /var/www/
 
+ln -s /worker/ /var/www/dist/webhook
+GITSHORT=`git rev-parse --short HEAD`
+
 if [ "$ANTIDOTE_ROLE" = "worker" ]; then
   echo "Running Migrations"
   php artisan migrate --force
 fi
-/var/www/scripts/notify.sh SUCCESS "Deploy happened on ${HOSTNAME}"
+
+/var/www/scripts/notify.sh SUCCESS "Deployed! Was Running ${OLD_GITSHORT} now running ${GITSHORT}, happened on ${HOSTNAME}"
