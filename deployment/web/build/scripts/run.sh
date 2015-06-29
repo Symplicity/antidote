b797d56db661c9d1c27ee9debaf850eda1a3be52
Richard <richard@guthnur.net>
2015-06-29 12:21:49 -0400

    Cleaned up the deployment folder and organized everything now that image building should be complete.

---
+++ deployment/web/build/scripts/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sh /setup.sh
+/sbin/my_init
--- deployment/worker/build/scripts/update.sh
+++ deployment/worker/build/scripts/update.sh
@@ -5,10 +5,11 @@ echo "Running Update Now\n"
 cd /var/www
 echo "Pulling Latest code from $GITREPO_BRANCH"
 git pull origin $GITREPO_BRANCH
+rm -rf /var/www/{dist,vendor}
 unzip -q dist.zip -d /var/www/
 
 if [ "$ANTIDOTE_ROLE" = "worker" ]; then
   echo "Running Migrations"
   php artisan migrate --force
 fi
-/var/www/scripts/notify.sh SUCCESS "Deploy happened."
+/var/www/scripts/notify.sh SUCCESS "Deploy happened on ${HOSTNAME}"
