988bdfa6c87550499901b7141f968fba4a36d7e5
Svemir Brkic <svemir@symplicity.com>
2015-06-29 23:11:54 -0400

    fix bug with first vote, add regression test

---
+++ tests/Http/Controllers/DrugReviewsControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use \App\Http\Controllers\DrugReviewsController;
+
+class DrugReviewsControllerTest extends TestCase
+{
+    /**
+     * @var DrugReviewsController
+     */
+    private $ctrl;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->ctrl = new DrugReviewsController;
+    }
+
+    public function testVote()
+    {
+        $this->setupDatabase();
+
+        $request = new Illuminate\Http\Request([
+            'user' => ['sub' => 'foo'],
+            'vote' => -1
+        ]);
+
+        $response = $this->ctrl->vote(1, $request);
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $vote = json_decode($response->getContent());
+        $this->assertEquals(-1, $vote->vote);
+        $vote_id = $vote->id;
+
+        $request = new Illuminate\Http\Request([
+            'user' => ['sub' => 'foo'],
+            'vote' => 1
+        ]);
+
+        $response = $this->ctrl->vote(1, $request);
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $vote = json_decode($response->getContent());
+        $this->assertEquals(1, $vote->vote, 'Repeated vote should override previous one');
+        $this->assertEquals($vote_id, $vote->id, 'Repeated vote should keep the id');
+
+        $request = new Illuminate\Http\Request([
+            'user' => ['sub' => 'bar'],
+            'vote' => 1
+        ]);
+
+        $response = $this->ctrl->vote(1, $request);
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $vote = json_decode($response->getContent());
+        $this->assertEquals(1, $vote->vote, 'Repeated vote should override previous one');
+        $this->assertNotEquals($vote_id, $vote->id, 'Vote by another user should be new');
+    }
+}
