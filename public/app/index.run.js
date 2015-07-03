fe4d08297077934a85221151d3212cfc67d8481c
Svemir Brkic <svemir@symplicity.com>
2015-07-02 23:33:03 -0400

    remove duplicated code by extending $mdToast

--- public/app/index.run.js
+++ public/app/index.run.js
@@ -6,7 +6,15 @@
         .run(runBlock);
 
     /** @ngInject */
-    function runBlock() {
+    function runBlock($mdToast) {
+        $mdToast.showSimple = function(message) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .content(message)
+                    .position('top right')
+                    .hideDelay(3000)
+            );
+        };
     }
 
 })();
