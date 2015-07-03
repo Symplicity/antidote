22473eeee4da0ead602e2a3738b8610c9fc63c97
Svemir Brkic <svemir@symplicity.com>
2015-07-03 00:00:35 -0400

    make toasts handle form validation messages

--- public/app/index.run.js
+++ public/app/index.run.js
@@ -8,6 +8,13 @@
     /** @ngInject */
     function runBlock($mdToast) {
         $mdToast.showSimple = function(message) {
+            if (typeof message === 'object') {
+                var toasts = [];
+                angular.forEach(message, function(m) {
+                    this.push(m);
+                }, toasts);
+                message = toasts.join(', ');
+            }
             $mdToast.show(
                 $mdToast.simple()
                     .content(message)
