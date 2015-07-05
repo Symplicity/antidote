3da87ef97303e3f08be08fe38caae2718113b8e9
Svemir Brkic <svemir@symplicity.com>
2015-07-05 10:24:25 -0400

    safer way to ensure all links are clicked

--- e2e/drugs.spec.js
+++ e2e/drugs.spec.js
@@ -6,15 +6,15 @@ describe('The drugs view', function() {
     beforeEach(function() {
         if (!page) {
             browser.manage().window().setSize(1024, 768);
-            // start with B to make sure we test a click on A
-            browser.get(browser.baseUrl + 'drugs/?term=b');
+            browser.get(browser.baseUrl);
             page = require('./main.po');
         }
     });
 
     it('should provide alphabetical navigation', function() {
+        page.menuButtons.get(1).click();
         var alphaLinks = element.all(by.repeater('letter in drugsList.letters'));
-        for (var i = 0; i < 26; i++) {
+        for (var i = 25; i >= 0; i--) {
             var link = alphaLinks.get(i);
             link.click();
             browser.waitForAngular();
