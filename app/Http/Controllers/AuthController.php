2de3a57b53538993c3abbc974932aa8e281cd105
Bikramjit Singh <bsingh@symplicity.com>
2015-07-06 18:41:03 -0400

    make link expiration time language match actual

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -139,7 +139,7 @@ class AuthController extends Controller
             'email' => $email,
             'site_url' => $site_url,
             'change_password_link' => $site_url . '/api/auth/reset/' . $token,
-            'link_expiration_time' => '90 Minutes'
+            'link_expiration_time' => '90 minutes'
         ];
 
         Mail::send('emails.forgot-password', $data, function ($message) use ($email) {
