286448fda2ffbeaa388b9f8e2cfc688471e0b091
Bikramjit Singh <bsingh@symplicity.com>
2015-07-06 15:31:00 -0400

    made the seeder use real drug data - limited to 50 on dev and full import on prod

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,9 +13,18 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $drug_import_args = [];
+        if (getenv('APP_ENV') == 'local') {
+            $drug_import_args = [
+                '--limit' => 50
+            ];
+        }
+
+        Artisan::call('import:drugs', $drug_import_args);
+
+        //drugs are now seeded above -
+        //for dev it will seed 50 drugs and for prod it will seed all drugs
         $this->call('UserSeeder');
-        $this->call('DrugIndicationSeeder');
-        $this->call('DrugSideEffectSeeder');
-        $this->call('DrugSeeder');
+        $this->call('DrugReviewSeeder');
     }
 }
